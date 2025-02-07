c7968775d5424cdb232e97508d0a65902628809f
Colin Gerlach <colinmgerlach@gmail.com>
2025-02-07 14:25:46 -0500

    2/7/25 edits

--- DataPlotting.py
+++ DataPlotting.py
@@ -13,18 +13,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+# Input absolute file path for the csv file extracted from DataAcquistion
 input_data1 = input("Path of extracted data: ")
 data0 = np.genfromtxt(input_data1, delimiter=',', names=True)
 
+# Create figure (parameters for labels, title, and legend can be edited as desired)
 plt.figure()
 mpl.rcParams.update({'font.size': 10})
-plt.xlabel("Engineering Strain  [%]")
-plt.ylabel("Engineering Stress [MPa]")
-plt.title("Strain Controlled Stress Hysteresis Loop CLAM steel")
+plt.xlabel("Strain [%]")
+plt.ylabel("Stress [MPa]")
+plt.title("Graph Title")
 plt.grid(True)
-plt.plot(data0['x'], data0['y'], color='r')
+plt.plot(data0['x'], data0['y'], color='r', label='Data')
+plt.legend(loc='best')
 
-
+# Saves output image in the output data images folder and names it the same as the input csv file
 output_name = os.path.splitext(os.path.basename(input_data1))[0]
 output_dir_name = "output data images"
 output_path = os.path.join(output_dir_name, f'{output_name}.png')
